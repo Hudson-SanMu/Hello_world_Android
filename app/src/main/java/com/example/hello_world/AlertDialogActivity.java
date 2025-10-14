0c8e51e7c384a45db58520111cb64be300d0c823
韩森宇 <hsy_916@qq.com>
2025-10-14 11:07:48 +0800

    多选弹窗

---
+++ app/src/main/java/com/example/hello_world/AlertDialogActivity.java
@@ -0,0 +1,48 @@
+package com.example.hello_world;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AlertDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_alert_dialog);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void showNormalDialog(View view) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("温馨提示")
+                .setMessage("确定要退出登录吗?")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(AlertDialogActivity.this, "取消", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(AlertDialogActivity.this, "确定", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .create();
+        alertDialog.show();
+    }
+}
